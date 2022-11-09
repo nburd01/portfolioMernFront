755cd2d3c15becf0b846f3e1b9fff01af351120f
nburd01 <n.burdon@protonmail.com>
2022-11-09 19:05:55 +0100

    connected to db

--- portfolio-front/server/index.js
+++ portfolio-front/server/index.js
@@ -2,3 +2,20 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+const app = express();
+
+app.use(bodyParser.json({limit:"30mb", extended:true }));
+app.use(bodyParser.urlencoded({limit:"30mb", extended:true }));
+app.use(cors());
+
+//https://www.mongodb.com/cloud/atlas
+
+const CONNECTION_URL = process.env.MONGO_CONNECTION_URL ;
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+// mongoose.set('useFindAndModify', false);
