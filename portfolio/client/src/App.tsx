061cdb213e1f62e647f8eef10e669de50380c498
nburd01 <n.burdon@protonmail.com>
2022-11-27 18:53:49 +0100

    added Apercu font

--- portfolio/client/src/App.tsx
+++ portfolio/client/src/App.tsx
@@ -3,6 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 import { Header } from './components/Pages/Header';
 import { Header2 } from './components/Pages/Header2';
+import './assets/fonts/ApercuPro-Medium.ttf'
+import './assets/fonts/ApercuPro-MediumItalic.ttf'
+import './assets/fonts/ApercuPro-ExtraLight.ttf'
+import './assets/fonts/ApercuPro-Black.ttf'
+import './assets/fonts/ApercuPro-Bold.ttf'
+import './assets/fonts/ApercuPro-BoldItalic.ttf'
+import './assets/fonts/ApercuPro-Thin.ttf'
+import './assets/fonts/ApercuPro-ThinItalic.ttf'
+import './assets/fonts/ApercuPro-Regular.ttf'
+import './assets/fonts/ApercuPro-Italic.ttf'
+import './assets/fonts/ApercuPro-ExtraLightItalic.ttf'
+import './assets/fonts/ApercuPro-Light.ttf'
+import './assets/fonts/ApercuPro-LightItalic.ttf'
+import './assets/fonts/ApercuPro-BlackItalic.ttf'
+
 
 function App() {
   return (
