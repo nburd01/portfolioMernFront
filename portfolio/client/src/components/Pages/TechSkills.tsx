64c1dd094554e5d16b078baa7f68ca6131d1ce27
nburd01 <n.burdon@protonmail.com>
2022-12-02 13:16:19 +0100

    added hover effects

--- portfolio/client/src/components/Pages/TechSkills.tsx
+++ portfolio/client/src/components/Pages/TechSkills.tsx
@@ -8,10 +8,10 @@ export const TechSkills = () => {
     return (
   <section className='container mx-auto px-4 h-2/3 ' id='TechSkills'>
     <div className='font-extrabold italic text-6xl md:text-9xl'>Tech Skills</div>
-    <div className='inline-bock sm:grid grid-cols-2 grid-flow-row gap-4'>
+    <div className='inline-block sm:place-items-center sm:grid grid-cols-2 grid-flow-row gap-4 hover:duration-300"'>
 
-        <div className="max-w-sm m-10 rounded overflow-hidden shadow-lg">
-          <div className="px-6 py-4 bg-beige">
+        <div className="w-2/3 m-10 rounded overflow-hidden shadow-lg">
+          <div className="px-6 py-4 bg-beige hover:bg-light_purple hover:duration-300">
             <div className="font-bold text-xl mb-2">Javascript</div>
             <img className="w-10 h-10 rounded-full mr-4 object-scale-down" src={JS} alt="Javascript"/>
           </div>
@@ -19,8 +19,8 @@ export const TechSkills = () => {
           </div>
         </div>
         
-        <div className="max-w-sm m-10 rounded overflow-hidden shadow-lg">
-          <div className="px-6 py-4 bg-beige">
+        <div className="w-2/3 m-10 rounded overflow-hidden shadow-lg">
+          <div className="px-6 py-4 bg-beige hover:bg-light_purple hover:duration-300">
             <div className="font-bold text-xl mb-2">React.js</div>
             <img className="w-10 h-10 rounded-full mr-4 object-scale-down" src={ReactPng} alt="React"/>
           </div>
@@ -28,8 +28,8 @@ export const TechSkills = () => {
           </div>
         </div>
 
-        <div className="max-w-sm m-10 rounded overflow-hidden shadow-lg">
-          <div className="px-6 py-4 bg-beige">
+        <div className="w-2/3 m-10 rounded overflow-hidden shadow-lg">
+          <div className="px-6 py-4 bg-beige hover:bg-light_purple hover:duration-300">
             <div className="font-bold text-xl mb-2">TypeScript</div>
             <img className="w-10 h-10 rounded-full mr-4 object-scale-down" src={TypeScript} alt="TypeScript"/>
           </div>
@@ -37,8 +37,8 @@ export const TechSkills = () => {
           </div>
         </div>
 
-        <div className="max-w-sm m-10 rounded overflow-hidden shadow-lg">
-          <div className="px-6 py-4 bg-beige">
+        <div className="w-2/3 m-10 rounded overflow-hidden shadow-lg">
+          <div className="px-6 py-4 bg-beige hover:bg-light_purple hover:duration-300">
             <div className="font-bold text-xl mb-2">Node.js</div>
             <img className="w-10 h-10 rounded-full mr-4 object-scale-down"  src={nodejs} alt="node.js"/>
           </div>
